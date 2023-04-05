9bc39f4309fa9ab6242e3345483ab1087b4ee13d
LiangRVR <liangvillarrubia95@gmail.com>
2023-04-05 14:37:51 -0400

    feat: create container component file, and implement simple container component

--- app/components/navbar/navbar.component.tsx
+++ app/components/navbar/navbar.component.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Container from "../container/container.component";
 
 const Navbar = () => {
   return (
     <div className="w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">Nav Bar</div>
+      <div className="py-4 border-b-[1px]">
+        <Container>container</Container>
+      </div>
     </div>
   );
 };
