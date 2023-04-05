c585b426405c9c0675bbf73f29f757198bab2b93
LiangRVR <liangvillarrubia95@gmail.com>
2023-04-05 14:53:46 -0400

    feat: implement logo component and call it in navbar component

--- app/components/navbar/navbar.component.tsx
+++ app/components/navbar/navbar.component.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Container from "../container/container.component";
+import Logo from "./logo.component";
 
 const Navbar = () => {
   return (
     <div className="w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-        <Container>container</Container>
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo/>
+          </div>
+        </Container>
       </div>
     </div>
   );
