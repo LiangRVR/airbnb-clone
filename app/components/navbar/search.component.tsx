180bc424ba3748dbe4469218d3d1ee80b553a8f4
LiangRVR <liangvillarrubia95@gmail.com>
2023-04-07 11:27:42 -0400

    add: create two new divs one inside the other one, each one with their classname

--- app/components/navbar/search.component.tsx
+++ app/components/navbar/search.component.tsx
@@ -3,7 +3,9 @@
 const Search = () => {
   return (
     <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor pointer">
-      Search
+      <div className="flex flex-row items-center justify-between">
+        <div className="text-sm font-semibold px-6">Something </div>
+      </div>
     </div>
   );
 };
