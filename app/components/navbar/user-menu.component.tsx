a6695eeb606294192a660a411836f3dee0ebf3d6
LiangRVR <liangvillarrubia95@gmail.com>
2023-04-07 12:02:09 -0400

    add(userMenu): add two mor div to define an structure, each one with classname style

--- app/components/navbar/user-menu.component.tsx
+++ app/components/navbar/user-menu.component.tsx
@@ -1,9 +1,18 @@
-'use client';
+"use client";
 
 const UserMenu = () => {
   return (
-    <div>UserMenu</div>
-  )
-}
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="hidden md:block text-sm font-semibold py-4 px-4  rounded-full hover:bg-neutral-100 transition cursor-pointer"
+        >
+          Airbnb your home
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default UserMenu
+export default UserMenu;
